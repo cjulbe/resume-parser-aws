dd45ce048ec4f0affde29db165a74d30c8c7aacd
Carlota Julbe <cjulbe26@gmail.com>
2025-11-21 16:55:13 +0000

    Update

--- api/app.py
+++ api/app.py
@@ -37,7 +37,7 @@ def upload_resume():
 
     # Clean up temp file
     if os.path.exists(file_path):
-    os.remove(file_path)
+        os.remove(file_path)
 
     s3_key = f"resumes/{os.path.splitext(filename)[0]}.json"
     s3_client.put_object(Bucket=S3_BUCKET, Key=s3_key, Body=str(parsed_data))
--- api/resume_parser.py
+++ api/resume_parser.py
@@ -1,7 +1,6 @@
 from docx import Document
 from pypdf import PdfReader
 
-
 def parse_resume(file_path):
     data = {
     "personal": {},
@@ -12,14 +11,12 @@ def parse_resume(file_path):
 }
         
     if file_path.endswith(".pdf"):
+        text = ""
         with open(file_path, 'rb') as f:
-            reader = PdfReader(f, strict=False)
-            text = ""
-
-            # To preserve internal formatting
+            reader = PdfReader(file_path, strict=False)
             for page in reader.pages:
                 extracted = page.extract_text() or ""
-                text += extracted + "\n" 
+                text += extracted + "\n"
     else:
         doc = Document(file_path)
         text = "\n".join([p.text for p in doc.paragraphs])
